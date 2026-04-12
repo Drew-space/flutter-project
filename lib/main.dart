8ba0ad7a7e1483623636567a86fefddbc2f6a044
Drew <chukwukaenudeme@gmail.com>
2026-04-12 20:04:37 +0100

    feat: Implement FirstPage with navigation to SecondPage and remove unused MyApp code

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/pages/first_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  List<String> names = ["Drew", "Tech", "Flutter"];
-
-  void printNames() {
-    print("Container tapped! check the console");
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App Bar', style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.lightBlue,
-          elevation: 0,
-          leading: Icon(Icons.menu, color: Colors.white),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.logout, color: Colors.white),
-            ),
-          ],
-        ),
-        body: Center(
-          child: GestureDetector(
-            onTap: printNames,
-            child: Container(
-              height: 200,
-              width: 200,
-              color: Colors.deepPurple[200],
-              child: Center(
-                child: Text(
-                  "Hello, World!",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: FirstPage());
   }
 }
--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/pages/second_page.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("First Page"), backgroundColor: Colors.blue),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+          child: Text("Go to Second Page"),
+        ),
+      ),
+    );
   }
 }
