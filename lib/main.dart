6c6924c6aba9735dc0ccbf0bced41a1a9788d6ca
Drew <chukwukaenudeme@gmail.com>
2026-04-13 16:08:04 +0100

    feat: Replace CounterPage with TodoPage and remove CounterPage implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/pages/counter_page.dart';
+
+import 'package:flutter_project/pages/todo_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: CounterPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: TodoPage());
   }
 }
