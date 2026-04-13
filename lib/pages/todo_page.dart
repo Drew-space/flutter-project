b17e8c92403bdb172e1b37a0c8ac85d64443df1f
Drew <chukwukaenudeme@gmail.com>
2026-04-13 16:31:12 +0100

    feat: Implement TodoPage with user input functionality and display todos

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -8,8 +8,44 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
+  TextEditingController myController = TextEditingController();
+
+  String todos = "";
+
+  void greetUser() {
+    setState(() {
+      todos = myController.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo App", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Text("Todo App"),
+              TextField(
+                controller: myController,
+                decoration: InputDecoration(
+                  hintText: "Enter todo item",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(onPressed: greetUser, child: Text("Add Todo")),
+              Text(todos),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
