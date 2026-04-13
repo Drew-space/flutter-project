5b8987d598f59f1f2eb97a12b55eaf3da6c02bc8
Drew <chukwukaenudeme@gmail.com>
2026-04-13 15:42:31 +0100

    feat: Convert CounterPage to StatefulWidget and implement counter functionality

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
-class CounterPage extends StatelessWidget {
+class CounterPage extends StatefulWidget {
   const CounterPage({super.key});
 
   @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  // variable
+  int _counter = 0;
+
+  //method
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter Page", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
       body: Center(
-        child: Text(
-          "Counter Page",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("You push the buttom this many times:"),
+            Text(
+              "$_counter",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Text("Increment!"),
+            ),
+          ],
         ),
       ),
     );
